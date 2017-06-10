c758c17c0e43f8c67ad672cb938d7e139c77c8ac
DeKal <hhphat@apcs.vn>
2017-06-10 22:19:38 +0700

    add some basic html to view data

--- web/src/main/java/com/jat/web/controller/WebController.java
+++ web/src/main/java/com/jat/web/controller/WebController.java
@@ -1,5 +1,7 @@
 package com.jat.web.controller; 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.jat.persistence.entity.Press;
 import com.jat.persistence.entity.VNExpressRootLink;
+import com.jat.service.PressService;
 import com.jat.service.VnExpressRootLinkService;
 
 import crawler.VnExpressBreadCrumbCrawlerController;
@@ -17,18 +22,22 @@ import crawler.VnExpressBreadCrumbCrawlerController;
 @Controller
 public class WebController {
 	
+	@Autowired
+	PressService pressService;
 	
-	@RequestMapping(value = "/a", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		
-		return "greeting";
-
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView homePage(ModelMap model) {
+		List<Press> lPress = pressService.listPress();
+		model.put("listPress", lPress );
+		return new ModelAndView("home", model);
 	}
-    @RequestMapping(value = "/testName", method = RequestMethod.GET)
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        System.out.print("run");
-        return "greeting";
+    @RequestMapping(value = "/getLinkInfo", method = RequestMethod.GET)
+    public ModelAndView greeting(@RequestParam(value="url", required=true) String url, ModelMap model) {
+       
+        Press press = pressService.findPress(url);
+        model.addAttribute("press", press);
+        return new ModelAndView("display-link", model);
+        
     }
 
 }
